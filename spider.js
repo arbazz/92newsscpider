639180ae68b6c3f273825184300fdd130d1c9680
arbazz <arbtouseef@gmail.com>
2022-06-07 19:00:46 +0500

    92 new

--- spider.js
+++ spider.js
@@ -1,23 +1,82 @@
 const puppeteer = require("puppeteer");
-
+const fs = require("fs");
 function delay(time) {
   return new Promise(function (resolve) {
     setTimeout(resolve, time);
   });
 }
-
+let index = 0;
+let elements = "";
 (async () => {
-  let classBtn = ".see-more";
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   await page.goto("https://92newshd.tv/latest-news");
-  for (let i = 0; i < 4200; i++) {
-    await delay(3000);
-    await page.waitForSelector(classBtn);
-    const btnAction = await page.evaluateHandle(() =>
-      document.querySelector(".btn-more")
-    );
-    await btnAction.evaluate((form) => form.click());
-    console.log("index0 ", i);
-  }
+  await getData(page, index);
 })();
+
+async function initiate(page, index) {
+  return new Promise(async (res) => {
+    const elements = await page.$$(".post-item");
+    res(elements);
+  });
+}
+async function getData(page) {
+  console.log("cuuret ->  ", index);
+  console.log("element length =>  ", elements.length);
+  await delay(2000);
+  let newTemp = await initiate(page);
+  console.log("new lenght ", newTemp.length);
+  if(newTemp.length <= index){
+    increaeTemp(page);
+    return;
+  }
+  elements = await page.$$(".post-item");
+  console.log("innder ======", elements.length);
+  let button = await elements[index];
+  await button.click();
+
+  await page.waitForSelector(".post-details");
+  const title = await page.$eval(
+    ".post-details h1",
+    (element) => element.innerHTML
+  );
+  console.log(title);
+  const desc = await page.$eval(
+    ".content_detail p:nth-child(2) ",
+    (element) => element.innerHTML
+  );
+  fs.appendFile("good.csv", title + "," + desc + "\r\n", (err) => {
+    if (err) console.log(err);
+    console.log('The "data to append" was appended to file!');
+  });
+  await page.goBack();
+  index += 1;
+  if (index >= elements.length) {
+    console.log("phir se");
+    // const btnAction = await page.evaluateHandle(() =>
+    //   document.querySelector(".btn-more")
+    // );
+    // console.log(btnAction);
+    // await btnAction.evaluate((form) => form.click());
+    let button = await page.$('.btn-more');
+    await button.click();
+    await delay(2000);
+    let newTemp = await initiate(page);
+    elements = newTemp;
+    console.log(elements.length);
+    getData(page);
+  } else {
+    getData(page);
+  }
+}
+
+const increaeTemp =async (page)=> {
+  
+    let button = await page.$('.btn-more');
+    await button.click();
+    await delay(2000);
+    let newTemp = await initiate(page);
+    elements = newTemp;
+    getData(page);
+  
+}
